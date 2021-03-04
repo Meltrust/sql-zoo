1b7b2df0fc741416afcc2c8c4d44163ef71b1dd7
meltrust <original.mtapia@outlook.com>
2021-03-04 09:10:14 -0600

    add sum_and_count

---
+++ SUM_and_COUNT.sql
@@ -0,0 +1,94 @@
+-- 'SUM and COUNT tutorial'
+--1
+SELECT
+  SUM(population)
+FROM
+  world;
+
+--2
+SELECT
+  DISTINCT continent
+FROM
+  world;
+
+--3
+SELECT
+  SUM(gdp)
+FROM
+  world
+WHERE
+  continent = 'Africa';
+
+--4
+SELECT
+  COUNT(name)
+FROM
+  world
+WHERE
+  area >= 1000000;
+
+--5
+SELECT
+  SUM(population)
+FROM
+  world
+WHERE
+  name IN ('Estonia', 'Latvia', 'Lithuania');
+
+--6
+SELECT
+  continent,
+  COUNT(name)
+FROM
+  world
+GROUP BY
+  continent;
+
+--7
+SELECT
+  continent,
+  COUNT(name)
+FROM
+  world
+WHERE
+  population >= 10000000
+GROUP BY
+  continent
+SELECT
+  continent,
+  COUNT(name)
+FROM
+  world
+WHERE
+  population >= 10000000
+GROUP BY
+  continent;
+
+--8
+SELECT
+  continent
+FROM
+  world
+GROUP BY
+  continent
+HAVING
+  SUM(population) >= 100000000;
+
+--QUIZ
+--1
+--'OPTION 3'
+--2
+--'Option 1'
+--3
+--'OPTION 4'
+--4
+--'OPTION 5'
+--5
+--'OPTION 2'
+--6
+--'OPTION 5'
+--7
+--'OPTION 4'
+--8
+--'OPTION 4'
+--END
